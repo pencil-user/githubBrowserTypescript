c9d69b1360f2d17dd30b0f42bfc82b984e87ad82
pencil-user <dbenasic@zoho.com>
2023-07-21 21:10:39 +0200

    use substring

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 export function useQueryParams(){
     const location = useLocation()
 
-    const queryParams = location.search.substr(1).split('&').reduce( (qs : any, query) => {
+    const queryParams = location.search.substring(1).split('&').reduce( (qs : any, query) => {
         const chunks = query.split('=');
         const key : any = chunks[0];
         let value : any = decodeURIComponent(chunks[1] || '');
